df81ca4c555e0ae34c8cd5dfeb87abb5acce4691
vectorTicK <tick1011@gmail.com>
2017-10-19 08:16:56 +0800

    first php

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Learn PHP
+### 1. index.php 第一个php文件
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>first php</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <?php
+        $txt1="Learn PHP";
+
+        echo "$txt1, my first php";
+        echo "<br>";
+?>
+    </body>
+</html>
+
+
